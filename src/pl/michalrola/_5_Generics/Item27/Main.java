adbfa50664d1d7e708c16dfb3ca5ebb764a7d23f
Michal Rola <michalrola@MacBook-Pro-Michal.local>
2021-05-05 19:55:39 +0200

    Item 27: Eliminate unchecked warnings

---
+++ src/pl/michalrola/_5_Generics/Item27/Main.java
@@ -0,0 +1,25 @@
+package pl.michalrola._5_Generics.Item27;
+
+import java.util.Arrays;
+
+public class Main {
+
+    private int size;
+    private Object[] elements;
+
+    // Adding local variable to reduce scope of @SuppressWarnings
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size) {
+            // This cast is correct because the array we're creating // is of the same type as the one passed in, which is T[].
+            @SuppressWarnings("unchecked") T[] result =
+                    (T[]) Arrays.copyOf(elements, size, a.getClass());
+            return result;
+        }
+        System.arraycopy(elements, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
+
+}
